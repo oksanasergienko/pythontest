68348dec1f4abd2d6f0b67b63b3902a4d4164081
Oksana <126798786+oksanasergienko@users.noreply.github.com>
2023-06-22 12:18:19 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-name = input("Введите имя: ")
-print("Привет,", name)
+name = input("Enter your name, bro: ")
+print("Hi,", name, ", glad to meet you!")
+x, y, z = "Just", "be", "Happy!"
+print(x)
+print(y)
+print(z)
+a = "Python"
+b = "is"
+c = "awesome!"
+print(a.upper(), b.upper(), c.upper())
