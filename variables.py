5a047c2ddbea4c6d9d850a656ae233488c9e088a
Oksana <126798786+oksanasergienko@users.noreply.github.com>
2023-06-22 12:19:05 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,24 @@
+variable_1 = 2
+variable_2 = 2
+print(variable_1 + variable_2)
+
+variable_1 = 4
+variable_2 = 5
+print(variable_1 * variable_2)
+
+str_1 = 'say'
+str_2 = 'my'
+str_3 = 'name'
+print(str_1 + ' ' + str_2 + ' ' + str_3)
+
+a = True
+b = False
+print('a: ', a)
+print('b: ', b)
+print('not a: ', not a)
+print('not b: ', not b)
+print('a and b: ', a and b)
+print('a or b:', a or b)
+print('1 ' + 'hi')
+
+print(type(12.5))
